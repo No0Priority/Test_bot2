f218af36edc6f0f44e0f8c79e6593de815b515c3
No0Priority <nidassar@gmail.com>
2022-05-06 00:42:41 +0300

    Merge remote-tracking branch 'origin/master'

--- test1/src/main/java/test1/test_echo.java
+++ test1/src/main/java/test1/test_echo.java
@@ -18,91 +18,41 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 
-public class test_echo extends TelegramLongPollingBot {
+public class test_echo extends TelegramLongPollingBot implements Runnable{
 
-
+	static List<WebElement> prices;
+	static Map<String, String> crypto_dict = new HashMap<String, String>();
 	public void onUpdateReceived(Update update) {
-		// TODO Auto-generated method stub
 	    if (update.hasMessage() && update.getMessage().hasText()) {
 	        //SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
 	        //message.setChatId(update.getMessage().getChatId().toString());
 	        //message.setText(update.getMessage().getText());
-	        
+	    	
 
 	            try {
 	            	SendMsg("1341282234", update.getMessage().getText());
-	            	SendMsg("844626806", update.getMessage().getText());
-
+	            	//SendMsg("844626806", update.getMessage().getText());
 	            	
-	            	System.out.println("before");
-	            	System.out.println("|"+update.getMessage().getText()+"|");
-	            	System.out.println(String.valueOf(update.getMessage().getText().equals("/test")));
-	            	System.out.println("after");
-	            	if (update.getMessage().getText().equals("/test")) {
+	            	if (update.getMessage().getText().equals("/start")) {
+	            		test_echo ts = new test_echo();
 	            		System.out.println("success");
-	            	// ===========INITIALIZATION==============
-	                //TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);// оставить в мейне
-	                //telegramBotsApi.registerBot(new test_echo()); //оставить в мейне
-	        		WebDriverManager.firefoxdriver().setup();
-	        		WebDriver driver = new FirefoxDriver();
-	        		driver.get("https://www.investing.com/crypto/");
-	        		test_echo ts = new test_echo(); 
-	        		// =========================
-	        		
-	        		//for (int i = 0; i<100; i++) {
-	        			Thread.sleep(5000);
-	        			//Elements Alex = doc.getElementsByClass("pid-1057391-last"); asdasd
-	        			//System.out.println(Alex.text());
-	        			WebElement bitcoin = driver.findElement(By.className("pid-1057391-last"));
-	        			List<WebElement> plants = driver.findElements(By.className("js-currency-price"));
-	        			System.out.println("before");
-	        			Map<String, String> crypto_dict = new HashMap<String, String>();
-
-	        			for (WebElement str:plants) {
-	        				crypto_dict.put(str.getAttribute("title"), str.getText());
-	        			}
-	        			System.out.println(Arrays.asList(crypto_dict));
-	        			String bitc = crypto_dict.get("BTC");
-	        			String eth = crypto_dict.get("ETH");
-	        			System.out.println("ETH"+eth+"BTC"+bitc);
-
-	        			System.out.println("after");
-	        			ts.SendMsg("1341282234", "курс битка: "+bitcoin.getText());
+	            		Thread thread = new Thread(ts);
+	            		thread.start();
+    				
+    				}
+    				
+	            	if (update.getMessage().getText().equals("/bitc")) {
+	            		System.out.println("bitc success");
+	            		String bitc = crypto_dict.get("BTC");
+	            		System.out.println(bitc);
+	        			SendMsg("1341282234", "курс битка: "+bitc);
 	        			
-	        			ts.SendMsg("844626806", "курс битка: "+bitcoin.getText());
-	        			driver.navigate().refresh();
-	        		//}
-
-	        		driver.quit();
-
-	        	
-	                /*
-	                while (true) {
-	                	Thread.sleep(1);
-	    	    		try {
-	    	    			Document doc = Jsoup.connect("https://wiki.narva.ut.ee/index.php?title=Sssd").get();
-	    	    			Elements Alex = doc.getElementsByClass("mw-parser-output");
-	    	    			String str = Alex.first().text();
-	    	    			if (str.equals("desired output")  || str.equals("Alex") || str.equals("iphone for 100 bucks")) {
-	    		    			System.out.println(str);
-	    		    			test_echo ts = new test_echo();
-	    		    			ts.SendMsg("1341282234", str);
-	    		    			
-	    		    			ts.SendMsg("844626806", str);
-	    		    			break;
-	    	    			}
-	    	    		}
-	    	    		catch (Exception e) {
-	    	    			System.out.println(e);
-	    	    		}
-	                }*/
+	        			//SendMsg("844626806", "курс битка: "+bitc);
 	            	}
 	            } catch (Exception e) {
 	                e.printStackTrace();
 	            }
-
 	    }
-		
 	}
 	public void SendMsg(String chat_id, String text) {
 		SendMessage message = new SendMessage(); 
@@ -124,6 +74,35 @@ public class test_echo extends TelegramLongPollingBot {
 	public String getBotToken() {
 		// TODO Auto-generated method stub
 		return "5394841904:AAEck1N7QG4kfKhW50_42MjWtjx-2jkex5U";
+	}
+	public void run() {
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.investing.com/crypto/");
+		
+		for (int i = 0; i<100; i++) {
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			WebElement bitcoin = driver.findElement(By.className("pid-1057391-last"));
+			prices = driver.findElements(By.className("js-currency-price"));
+			System.out.println("before");
+			
+
+			for (WebElement str:prices) {
+				crypto_dict.put(str.getAttribute("title"), str.getText());
+			}
+			System.out.println(Arrays.asList(crypto_dict));
+			String bitc = crypto_dict.get("BTC");
+			String eth = crypto_dict.get("ETH");
+			System.out.println("ETH"+eth+"BTC"+bitc);
+			driver.navigate().refresh();
+		}
+		
 	}
 
 }
