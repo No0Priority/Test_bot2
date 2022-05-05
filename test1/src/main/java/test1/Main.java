b9cbe08514b446b922602167ded86a2711c85fd6
No0Priority <nidassar@gmail.com>
2022-05-05 14:28:03 +0300

    14:26

--- test1/src/main/java/test1/Main.java
+++ test1/src/main/java/test1/Main.java
@@ -24,7 +24,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 
 public class Main {
-    // change 14:25
+    // change 14:26
     public static void main(String[] args) throws InterruptedException {
     	PropertyConfigurator.configure(System.getProperty("user.dir") + "/src/main/resources/log4j.properties");
 
