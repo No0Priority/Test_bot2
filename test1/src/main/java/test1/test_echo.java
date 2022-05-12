eddb956969a566536690c3cac8d2730b7b4a9be6
mikro <mikro@DESKTOP-USKVP4O>
2022-05-12 15:03:33 +0300

    upd

--- test1/src/main/java/test1/test_echo.java
+++ test1/src/main/java/test1/test_echo.java
@@ -1,4 +1,5 @@
 package test1;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -9,8 +10,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
@@ -24,32 +29,65 @@ public class test_echo extends TelegramLongPollingBot implements Runnable{
 	static Map<String, String> crypto_dict = new HashMap<String, String>();
 	public void onUpdateReceived(Update update) {
 	    if (update.hasMessage() && update.getMessage().hasText()) {
-	        //SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
-	        //message.setChatId(update.getMessage().getChatId().toString());
-	        //message.setText(update.getMessage().getText());
+	        SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+	        message.setChatId(update.getMessage().getChatId().toString());
+	        message.setText(update.getMessage().getText());
 	    	
 
 	            try {
-	            	SendMsg("1341282234", update.getMessage().getText());
-	            	//SendMsg("844626806", update.getMessage().getText());
+	            	//SendMsg("1341282234", update.getMessage().getText());
+	            	SendMsg("844626806", update.getMessage().getText());
 	            	
 	            	if (update.getMessage().getText().equals("/start")) {
+	            		SendMessage sendMessage = new SendMessage();
+	            		sendMessage.setText("Hello, which cryptocurrency would you like to track?");
+	            		sendMessage.setParseMode(ParseMode.MARKDOWN);
+	            		sendMessage.setChatId(message.getChatId());
 	            		test_echo ts = new test_echo();
-	            		System.out.println("success");
+	            		ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+	            		replyKeyboardMarkup.setResizeKeyboard(true);
+	            		List<KeyboardRow> keyboardRowList = new ArrayList<>();
+	            		KeyboardRow keyboardRow1 = new KeyboardRow();
+	            		keyboardRow1.add("Bitcoin");
+	            		keyboardRow1.add("Ethereum");
+	            		keyboardRow1.add("Tether");
+	            		keyboardRowList.add(keyboardRow1);
+	            		replyKeyboardMarkup.setKeyboard(keyboardRowList);
+	            		sendMessage.setReplyMarkup(replyKeyboardMarkup);
+	            		execute(sendMessage);
 	            		Thread thread = new Thread(ts);
 	            		thread.start();
     				
     				}
     				
-	            	if (update.getMessage().getText().equals("/bitc")) {
+	            	else if (update.getMessage().getText().equals("Bitcoin")) {
 	            		System.out.println("bitc success");
 	            		String bitc = crypto_dict.get("BTC");
 	            		System.out.println(bitc);
-	        			SendMsg("1341282234", "курс битка: "+bitc);
+	        			//SendMsg("1341282234", "Bitcoin: "+bitc);
 	        			
-	        			//SendMsg("844626806", "курс битка: "+bitc);
+	        			SendMsg("844626806", "Bitcoin: "+bitc);
 	            	}
-	            } catch (Exception e) {
+	        			
+	            	else if (update.getMessage().getText().equals("Ethereum")) {
+		            	System.out.println("ethereum success");
+		            	String eth = crypto_dict.get("ETH");
+		            	System.out.println(eth);
+		        		//SendMsg("1341282234", "Ethereum: "+eth);
+		        		SendMsg("844626806", "Ethereum: "+eth);
+	        		}
+		        
+	            	else if (update.getMessage().getText().equals("Tether")) {
+		            	System.out.println("tether success");
+		            	String usdt = crypto_dict.get("USDT");
+		            	System.out.println(usdt);
+		        		//SendMsg("1341282234", "Tether: "+usdt);
+		        			
+		        		SendMsg("844626806", "Tether: "+usdt);
+	            	}
+	            	
+	        		
+	        	} catch (Exception e) {
 	                e.printStackTrace();
 	            }
 	    }
